b4f5fb026b7edc4b9d1af45416093eb4e21a1bd7
Henry <henrysg88@gmail.com>
2021-05-24 21:39:36 -0400

    feat(Main): Remove default nest js files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BullModule } from '@nestjs/bull';
+import { TranscodeProcessor } from './transcode-processor';
+import { FfmpegTanscoder } from './infrastructure/transcoder';
+import { S3HlsStorageManager } from './infrastructure/s3-hls-storage';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    BullModule.registerQueue({
+      name: process.env.QUEUE_NAME,
+      redis: {
+        host: process.env.QUEUE_HOST,
+        port: parseInt(process.env.QUEUE_PORT),
+        username: process.env.QUEUE_USERNAME,
+        password: process.env.QUEUE_PASSWORD,
+      },
+    }),
+  ],
+
+  providers: [
+    { provide: 'ITranscoder', useClass: FfmpegTanscoder },
+    { provide: 'IHlsStorageManager', useClass: S3HlsStorageManager },
+    TranscodeProcessor,
+  ],
 })
 export class AppModule {}
